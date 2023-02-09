1c17ad615972586a117f1af0dfe728e11cb769ab
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-02-09 15:14:24 +0100

    fix: unused var

--- packages/eslint-config-bases/src/bases/mdx.js
+++ packages/eslint-config-bases/src/bases/mdx.js
@@ -11,7 +11,7 @@ module.exports = {
   overrides: [
     {
       // For performance enable this only on mdx files
-      files: ['*.mdx'],
+      files: mdxPatterns,
       extends: ['plugin:mdx/recommended'],
       parser: 'eslint-mdx',
       rules: {
