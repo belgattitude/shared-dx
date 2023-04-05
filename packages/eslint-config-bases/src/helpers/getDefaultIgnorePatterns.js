4545ba5537b66b716946129bcc6f8d9da47be148
Sébastien Vanvelthem <belgattitude@users.noreply.github.com>
2023-04-05 15:20:50 +0200

    feat: add storybook-static and .yarn to default ignores (#198)
    * feat: add storybook-static and .yarn to default ignores
    
    * feat: add storybook-static and .yarn to default ignores

--- packages/eslint-config-bases/src/helpers/getDefaultIgnorePatterns.js
+++ packages/eslint-config-bases/src/helpers/getDefaultIgnorePatterns.js
@@ -1,6 +1,13 @@
 const getDefaultIgnorePatterns = () => {
   // Hacky way to silence @yarnpkg/doctor about node_modules detection
-  return [`**/${'node'}_modules}`, '**/.cache', 'build', 'dist'];
+  return [
+    `**/${'node'}_modules}`,
+    '**/.cache',
+    'build',
+    'dist',
+    'storybook-static',
+    '.yarn',
+  ];
 };
 
 module.exports = {
