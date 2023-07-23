048e66bfcf91f848c84f12ed5dcbe56bda1f25e9
Sébastien Vanvelthem <belgattitude@users.noreply.github.com>
2023-07-23 12:16:14 +0200

    feat: add .turbo and .out in default ignored patterns (#298)
    * feat: add .turbo and .out in default ignored patterns
    
    * feat: add .turbo and .out in default ignored patterns
    
    * feat: add .turbo and .out in default ignored patterns

---
+++ .changeset/hip-actors-promise.md
@@ -0,0 +1,5 @@
+---
+'@belgattitude/eslint-config-bases': minor
+---
+
+Add .turbo and .out as default ignored patterns
--- packages/eslint-config-bases/src/helpers/getDefaultIgnorePatterns.js
+++ packages/eslint-config-bases/src/helpers/getDefaultIgnorePatterns.js
@@ -1,12 +1,16 @@
 const getDefaultIgnorePatterns = () => {
   // Hacky way to silence @yarnpkg/doctor about node_modules detection
   return [
+    `${'node'}_modules}`,
     `**/${'node'}_modules}`,
     '**/.cache',
     'build',
     'dist',
     'storybook-static',
     '.yarn',
+    '.turbo',
+    `**/.turbo`,
+    '.out',
   ];
 };
 
