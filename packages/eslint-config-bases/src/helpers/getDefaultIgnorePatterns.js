40c73d8372c155430258f87c9b2a925da1f49988
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-07-23 12:04:25 +0200

    feat: add .turbo and .out in default ignored patterns

--- packages/eslint-config-bases/src/helpers/getDefaultIgnorePatterns.js
+++ packages/eslint-config-bases/src/helpers/getDefaultIgnorePatterns.js
@@ -1,6 +1,7 @@
 const getDefaultIgnorePatterns = () => {
   // Hacky way to silence @yarnpkg/doctor about node_modules detection
   return [
+    `${'node'}_modules}`,
     `**/${'node'}_modules}`,
     '**/.cache',
     'build',
@@ -8,6 +9,7 @@ const getDefaultIgnorePatterns = () => {
     'storybook-static',
     '.yarn',
     '.turbo',
+    `**/.turbo`,
     '.out'
   ];
 };
