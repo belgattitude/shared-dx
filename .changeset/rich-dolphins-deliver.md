b1ff7cb2f4d6179bf1f7e65637214fc1e42ae838
Sébastien Vanvelthem <belgattitude@gmail.com>
2024-04-01 11:46:36 +0200

    chore: revert typescript eslint v7

--- .changeset/rich-dolphins-deliver.md
+++ .changeset/rich-dolphins-deliver.md
@@ -2,4 +2,23 @@
 "@belgattitude/eslint-config-bases": major
 ---
 
-Typoescript eslint v7, drop react query eslint plugin < 5
+Typescript eslint v7.4, drop react query eslint plugin < 5.
+
+If you encounter issues like 'ESLint couldn't determine the plugin "import" uniquely.'. Ensure
+that other plugins are on typescript-eslint v7 as well. 
+
+> For Next.js users, the Next.js eslint config < 14.2.0 doesn't support typescript-eslint v7.
+> See this PR: https://github.com/vercel/next.js/pull/62137. To allow v7 either wait for 
+> 14.2.0 to be releases or override the package version
+[PR]
+
+```json
+{
+  "resolutions": {
+    "@typescript-eslint/eslint-plugin": "^7.4.0",
+    "@typescript-eslint/parser": "^7.4.0",
+    "@typescript-eslint/typescript-estree": "^7.4.0",
+    "@typescript-eslint/utils": "^7.4.0"
+  }
+}
+```
--- examples/nextjs-app/.storybook/main.ts
+++ examples/nextjs-app/.storybook/main.ts
@@ -1,49 +1,30 @@
 import type { StorybookConfig } from '@storybook/nextjs';
 
+import { join, dirname } from 'path';
+
+/**
+ * This function is used to resolve the absolute path of a package.
+ * It is needed in projects that use Yarn PnP or are set up within a monorepo.
+ */
+function getAbsolutePath(value: string): any {
+  return dirname(require.resolve(join(value, 'package.json')));
+}
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
+    getAbsolutePath('@storybook/addon-onboarding'),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@chromatic-com/storybook'),
+    getAbsolutePath('@storybook/addon-interactions'),
   ],
   framework: {
-    name: '@storybook/nextjs',
-    options: {
-      strictMode: true,
-      nextConfigPath: '../next.config.mjs',
-    },
+    name: getAbsolutePath('@storybook/nextjs'),
+    options: {},
   },
-  /*
-  babel: (config) => {
-    config.presets.push(require.resolve('@emotion/babel-preset-css-prop'));
-    return config;
-  },*/
-
-  // https://github.com/storybookjs/storybook/tree/next/code/frameworks/nextjs#custom-webpack-config
-  /*
-  webpackFinal: async (config) => {
-    // This modifies the existing image rule to exclude .svg files
-    // since you want to handle those files with @svgr/webpack
-    const imageRule = config.module.rules.find((rule) =>
-      rule.test.test('.svg')
-    );
-    imageRule.exclude = /\.svg$/;
-
-    // Configure .svg files to be loaded with @svgr/webpack
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-
-    return merge(config, {
-      plugins: [new VanillaExtractPlugin()],
-    });
-  },*/
-
   docs: {
     autodocs: 'tag',
-    defaultName: 'Docs', // set to change the name of generated docs entries
   },
+  staticDirs: ['../public'],
 };
 export default config;
