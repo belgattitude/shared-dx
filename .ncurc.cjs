2c18fc8e23d66cdb7a90d4ccb8c551b8dec38dd5
Sebastien Vanvelthem <belgattitude@gmail.com>
2025-10-16 10:01:56 +0200

    chore: minimal age gate to 3 days

---
+++ .ncurc.cjs
@@ -0,0 +1,32 @@
+// @ts-check
+
+const { defineConfig } = require('npm-check-updates');
+
+// @todo read the content from .yarnrc.yml
+const npmPreapprovedPackagesPrefixes = [
+  '@belgattitude/',
+  '@flowblade/',
+  '@httpx/',
+];
+
+module.exports = defineConfig({
+  workspaces: true,
+  mergeConfig: true,
+  root: true,
+  packageManager: 'yarn',
+  cooldown: (packageName) => {
+    if (
+      npmPreapprovedPackagesPrefixes.some((prefix) =>
+        packageName.startsWith(prefix)
+      )
+    ) {
+      return 0;
+    }
+    return 3;
+  },
+  reject: [
+    // Cause v9 isn't yet supported
+    'eslint',
+    'eslint-plugin-unicorn',
+  ],
+});
